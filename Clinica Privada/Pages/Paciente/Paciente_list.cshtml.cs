641534d1dfb2407ced5885c14ec545d077cfeebf
Raúl Alfaro <125586767+RaJami1205@users.noreply.github.com>
2024-09-30 21:33:36 -0600

    Creación de la interfaz de la lista y del formulario para el paciente y el personal de salud.

---
+++ Clinica Privada/Pages/Paciente/Paciente_list.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Clinica_Privada.Pages.Paciente
+{
+    public class Paciente_listModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Clinica Privada/Pages/Personal/Personal_form.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Clinica_Privada.Pages.Personal
+{
+    public class Personal_formModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
